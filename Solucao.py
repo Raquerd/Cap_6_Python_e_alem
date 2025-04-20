dffcc1e8860a253d0225cbce40aba2f7ae5cc17c
Davi Ferreira <davistudyreference@gmail.com>
2025-04-20 15:05:53 -0300

    MOD: Adicionado tratativa com arquivos externos

--- Solucao.py
+++ Solucao.py
@@ -1,4 +1,4 @@
-import oracledb, pandas as pd, os
+import oracledb, pandas as pd, os, json
 from time import sleep
 
 # try:
@@ -16,52 +16,77 @@ df = {'INSUMOS':[], 'CATEGORIA':[], 'FABRICANTE':[], 'VALIDADE':[], 'QTDE_EM_EST
 print(df)
 def cadastro(dataframe):
     cursor = conn.cursor()
-    cursor.execute(f'''INSERT INTO 
+    cursor.executemany(f'''INSERT INTO 
                         INSUMOS_AGRIGOLAS (
-                            id,
-                            insumo, 
-                            categoria, 
-                            fabricante, 
-                            validade, 
-                            qtde_em_estoque, 
-                            unidade_de_medida, 
-                            aquisição, 
-                            valor_unitário)
-                        VALUES({dataframe})
-                   ''')
+                            INSUMO, 
+                            CATEGORIA, 
+                            FABRICANTE, 
+                            VALIDADE, 
+                            QTDE_EM_ESTOQUE, 
+                            UNIDADE_DE_MEDIDA, 
+                            AQUISICAO, 
+                            VALOR_UNITARIO)
+                        VALUES(:1, :2, :3, :4, :5, :6, :7, :8)
+                   ''', dataframe.itertuples(index=False, name=None))
+    conn.commit()
+    conn.close()
  
 while True:
     menu_option = int(input(f'''{'-'*15}
-                            Seleciona a ação que deseja realizar
-                        
-                            [1] Cadastrar novo insumo
-                            [2] Consultar insumos
-                            [3] Atualizar insumo
-                            [4] Excluir insumo
-                            [5] Sair
-                            {'-'*15}
+Seleciona a ação que deseja realizar
+
+[1] Cadastrar novo insumo
+[2] Consultar insumos
+[3] Atualizar insumo
+[4] Excluir insumo
+[5] Sair
+{'-'*15}
 '''))
     match menu_option:
         case 1:
-            condition = int(input("Você possui um arquivo formatado com os dados que deseja inserir?:\n [1] SIM\n [2]NAO\nDigite: "))
+            condition = int(input("Você possui um arquivo formatado com os dados que deseja inserir?:\n [1] SIM\n [2] NAO\nDigite: "))
             if condition == 1:
                 try:
+                    print('O Arquivo deve conter as colunas estrutradas na seguinte ordem:\nINSUMO, CATEGORIA, FABRICANTE, VALIDADE, QUANTIDADE EM ESTOQUE, UNIDADE DE MEDIDA, DATA DE AQUISICAO, VALOR UNITARIO\n')
                     path = input('Digite aqui o diretório do arquivo: ')
-                    with open(path, 'r') as archive:
-                        archive.read()
+                    with open(rf'{path}', 'r') as archive:
+                        df = json.load(archive)
+                    df = pd.DataFrame(df)
+                    df.columns = ['INSUMO', 'CATEGORIA', 'FABRICANTE', 'VALIDADE', 'QTDE_EM_ESTOQUE', 'UNIDADE_DE_MEDIDA', 'AQUISICAO', 'VALOR_UNITARIO']
                 except:
-                    print('erro')
-            df['INSUMOS'] = input("Digite o insumo que deseja inserir: ")
-            df['CATEGORIA'] = input("Digite a categoria do insumo inserido: ")
-            df['FABRICANTE'] = input("Digite quem é o fabricante deste insumo: ")
-            df['VALIDADE'] = input("Digite a validade do insumo: ")
-            df['QTDE_EM_ESTOQUE'] = float(input("Digite a quantidade em estoque deste insumo: "))
-            df['UNIDADE_DE_MEDIDA'] = input("Digite qual a unidade de medida adequada para este insumo: ")
-            df['AQUISICAO'] = input("Digite a data de aquisição do insumo: ")
-            df['VALOR_UNITARIO'] = float(input("Digite o valor unitário do insumo: "))
-            os.system('cls')
-            sleep()
+                    print('Algo não saiu como esperado, tente novamente.')
+                else:
+                    print(df)
+                    # cadastro(df)
+            else:
+                df['INSUMO'] = input("Digite o insumo que deseja inserir: ")
+                df['CATEGORIA'] = input("Digite a categoria do insumo inserido: ")
+                df['FABRICANTE'] = input("Digite quem é o fabricante deste insumo: ")
+                df['VALIDADE'] = input("Digite a validade do insumo: ")
+                df['QTDE_EM_ESTOQUE'] = float(input("Digite a quantidade em estoque deste insumo: "))
+                df['UNIDADE_DE_MEDIDA'] = input("Digite qual a unidade de medida adequada para este insumo: ")
+                df['AQUISICAO'] = input("Digite a data de aquisição do insumo: ")
+                df['VALOR_UNITARIO'] = float(input("Digite o valor unitário do insumo: "))
 
+            cursor = conn.cursor()
+            cursor.executemany(f'''INSERT INTO 
+                                INSUMOS_AGRIGOLAS (
+                                    INSUMO, 
+                                    CATEGORIA, 
+                                    FABRICANTE, 
+                                    VALIDADE, 
+                                    QTDE_EM_ESTOQUE, 
+                                    UNIDADE_DE_MEDIDA, 
+                                    AQUISICAO, 
+                                    VALOR_UNITARIO)
+                                VALUES(:1, :2, :3, :4, :5, :6, :7, :8)
+                        ''', df.itertuples(index=False, name=None))
+            conn.commit()
+            conn.close()
+ 
+            input('Pressione ENTER para prosseguir para o MENU.')
+            os.system('cls')
+            
         case 2:
             df_cosulta = pd.read_sql('SELECT * FROM INSUMOIS_AGRICOLAS')
             print(df_cosulta)
