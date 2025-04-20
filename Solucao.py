b8689d4ad7c986f6a2c5c9aed0cfffe84359657d
Davi Ferreira <davistudyreference@gmail.com>
2025-04-20 19:08:58 -0300

    Implementando possibilidade de consulta por lote

--- Solucao.py
+++ Solucao.py
@@ -13,9 +13,8 @@ oracledb.init_oracle_client(lib_dir=r'C:\Users\Davi\Documents\instantclient-basi
 conn = oracledb.connect(user="sys", password="090402", dsn='localhost:1521/XEPDB1', mode=oracledb.SYSDBA)
 cursor = conn.cursor()
 # aaaaa
-df = {'INSUMOS':[], 'CATEGORIA':[], 'FABRICANTE':[], 'VALIDADE':[], 'QTDE_EM_ESTOQUE':[], 'UNIDADE_DE_MEDIDA':[], 'AQUISICAO':[], 'VALOR_UNITARIO':[]}
-print(df)
-def cadastro(dataframe):
+
+def cadastro(dataframe, condition):
     cursor.executemany(f'''INSERT INTO INSUMOS_AGRICOLAS (
                             ID,
                             INSUMO, 
@@ -28,6 +27,7 @@ def cadastro(dataframe):
                             VALOR_UNITARIO)
                         VALUES(SEQ_CLIENTE.NEXTVAL, :1, :2, :3, :4, :5, :6, :7, :8)
                 ''', list(dataframe.itertuples(index=False, name=None)))
+
     conn.commit()
 
 def delete():
@@ -37,6 +37,7 @@ def delete():
 
  
 while True:
+    df_list = []
     menu_option = int(input(f'''{'-'*15}
 Seleciona a ação que deseja realizar
 
@@ -47,6 +48,14 @@ Seleciona a ação que deseja realizar
 [5] Sair
 {'-'*15}
 '''))
+    
+    if menu_option > 5 or menu_option < 1:
+        os.system('cls')
+        print("Ops, algo deu errado\nDigite uma opção valida")
+        sleep(3)
+        os.system('cls')
+        continue
+
     match menu_option:
         case 1:
             condition = int(input("Você possui um arquivo formatado com os dados que deseja inserir?:\n [1] SIM\n [2] NAO\nDigite: "))
@@ -58,38 +67,91 @@ Seleciona a ação que deseja realizar
                         df = json.load(archive)
                     df = pd.DataFrame(df)
                     df.columns = ['INSUMO', 'CATEGORIA', 'FABRICANTE', 'VALIDADE', 'QTDE_EM_ESTOQUE', 'UNIDADE_DE_MEDIDA', 'AQUISICAO', 'VALOR_UNITARIO']
-                    # df['ID'] = df.index
-                    # df = df[['ID','INSUMO', 'CATEGORIA', 'FABRICANTE', 'VALIDADE', 'QTDE_EM_ESTOQUE', 'UNIDADE_DE_MEDIDA', 'AQUISICAO', 'VALOR_UNITARIO']]
+
                 except:
                     print('Algo não saiu como esperado, tente novamente.')
-                else:
-                    print(df)
 
-            else:
-                df['INSUMO'] = input("Digite o insumo que deseja inserir: ")
-                df['CATEGORIA'] = input("Digite a categoria do insumo inserido: ")
-                df['FABRICANTE'] = input("Digite quem é o fabricante deste insumo: ")
-                df['VALIDADE'] = input("Digite a validade do insumo: ")
-                df['QTDE_EM_ESTOQUE'] = float(input("Digite a quantidade em estoque deste insumo: "))
-                df['UNIDADE_DE_MEDIDA'] = input("Digite qual a unidade de medida adequada para este insumo: ")
-                df['AQUISICAO'] = input("Digite a data de aquisição do insumo: ")
-                df['VALOR_UNITARIO'] = float(input("Digite o valor unitário do insumo: "))
-
-            cadastro(df)
+            elif condition == 2:
+                while True:
+                    registro = {
+                        'INSUMO': input("Digite o insumo que deseja inserir: "),
+                        'CATEGORIA':input("Digite a categoria do insumo inserido: "),
+                        'FABRICANTE':input("Digite quem é o fabricante deste insumo: "),
+                        'VALIDADE':input("Digite a validade do insumo: "),
+                        'QTDE_EM_ESTOQUE':float(input("Digite a quantidade em estoque deste insumo: ")),
+                        'UNIDADE_DE_MEDIDA':input("Digite qual a unidade de medida adequada para este insumo: "),
+                        'AQUISICAO':input("Digite a data de aquisição do insumo: "),
+                        'VALOR_UNITARIO':float(input("Digite o valor unitário do insumo: "))
+                    }
+                    df_list.append(registro)
+                    resp = float(input('Gostaria de inserir mais dados ?:\n[1] SIM\n[2] NAO\nDigite: '))
+                    if resp == 2:
+                        break
+                df = pd.DataFrame(df_list)
+            cadastro(df, condition)
  
             input('Pressione ENTER para prosseguir para o MENU.')
             os.system('cls')
             
         case 2:
-            df_cosulta = pd.read_sql('SELECT * FROM INSUMOIS_AGRICOLAS')
+            os.system('cls')
+            option = int(input('Qual o tipo de consulta que gostaria de realizar?\n[1] Consulta de registros por lote\n[2] Consulta de registro unico\nDigite aqui: '))
+            match option:
+                case 1:
+                    data_1 = ''
+                    data_2 = ''
+                    insumos = ''
+                    fabricante = ''
+                    categoria = ''
+                    sql = 'SELECT * FROM INSUMOS_AGRICOLAS WHERE'
+
+                    while True:
+                        os.system('cls')
+                        ask = int(input('''Quais filtros gostaria de fazer ?\n[1] Filtro de data de valiade\n[2] Filtro de insumos\n[3] Filtro de fabricante\n[4] Filtro de categoria\n\nLEMBRETE: Caso queira refazer algum filtro ja feito, os dados serão sobrepostos\nDigite aqui: '''))
+                        match ask:
+                            case 1:
+                                data_1 = input('Digite a data de validade inicial: ')
+                                data_2 = input('Digite a data de validade final: ')
+                                sql = sql + f" VALIDADE >= {data_1} and VALIDADE <= {data_2}" if 'and' not in sql else sql + f" and VALIDADE >= {data_1} and VALIDADE <= {data_2}"
+                            case 2:
+                                insumos = input('Digite separando cada elemento por "," quais insumos deseja analisar: ').strip()
+                                sql = sql + (" INSUMO in (" + "'" + "', '".join([i for i in insumos.split(',')]) + "')") if 'and' not in sql else sql + (" and INSUMO in (" + "'" + "', '".join([i for i in insumos.split(',')]) + "')")
+                            case 3:
+                                fabricante = pd.read_sql('SELECT DISTINCT FABRICANTE FROM INSUMOS_AGRICOLAS', conn)
+                                # input(f'''Digite separando cada elemento por "," quais fabricantes deseja consultar.\nLista de fabricantes\n{pd.read_sql('SELECT DISTINCT FABRICANTE FROM INSUMOS_AGRICOLAS', conn)}\nDigite aqui:''').strip()
+                                fabricante_list = []
+                                while True:
+                                    fabricante_list.append(fabricante.loc[int(input(f'''Digite o numero referente a qual fabricantes deseja consultar.\nLista de fabricantes\n{fabricante}\nDigite aqui:'''))])
+                                    print(fabricante_list)
+                                    if int(input("Deseja inserir mais algum fabricane na consulta?\n[1] SIM\n[2] NAO\nDigite: ")) == 2:
+                                        break
+                                sql = sql + (" FABRICANTE in (" + "'" + "', '".join([i for i in fabricante_list]) + "')") if 'and' not in sql else sql + (" and FABRICANTE in (" + "'" + "', '".join([i for i in fabricante_list]) + "')")
+                                print(sql)
+                            case 4:
+                                categoria = input('Digite separando cada elemento por "," quais fabricantes deseja consultar: ').strip()
+                                sql = sql + (" CATEGORIA in (" + "'" + "', '".join([i for i in categoria.split(',')]) + "')") if 'and' not in sql else sql + (" and CATEGORIA in (" + "'" + "', '".join([i for i in categoria.split(',')]) + "')")
+                        if int(input('Gostaria de adicionar mais dados\n[1] SIM\n[2] NAO\nDigite: ')) == 2:
+                            # os.system('cls')
+                            print(F"INSUMOS: {insumos}\nDATA: {data_1} - {data_2}\nFABRICANTE: {fabricante_list}\nCATEGORIA: {categoria}")
+                            break
+                case 2:
+                    id_consulta = int(input("Insira o ID do registro que deseja vizualizar: "))
+
+            df_cosulta = pd.read_sql(sql, conn)
             print(df_cosulta)
 
             input('Pressione ENTER para prosseguir para o MENU.')
             os.system('cls')
 
         
-        # case 4:
-            # cursor = conn.cursor()
-            # cursor.execute("DELETE FROM INSUMOS_AGRICOLAS")
-            # conn.commit()
-    # conn.close()
+        case 4:
+            os.system('cls')
+            id = int(input("Digite o ID do insumo que deseja eliminar do banco de dados: "))
+            cursor = conn.cursor()
+            cursor.execute(F"DELETE FROM INSUMOS_AGRICOLAS WHERE ID = {id}")
+            conn.commit()
+            os.system('cls')
+
+        case 5:
+            conn.close()
+            exit()
